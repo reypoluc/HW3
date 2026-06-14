e08d2215a0c36baf8a1ad92ba2e453f9542f8f22
reypoluc <ermolaevap08@gmail.com>
2026-06-14 12:18:18 +0000

    написала башскрипт

--- run.sh
+++ run.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+build_generator() {
+    docker build -t data-generator ./generator
+}
+
+run_generator() {
+    docker run --rm -v "$(pwd)/data:/data" data-generator
+}
+
+create_local_data() {
+    mkdir -p data
+    python3 generator/generate.py data
+}
+
+case "$1" in
+    build_generator) build_generator ;;
+    run_generator) run_generator ;;
+    create_local_data) create_local_data ;;
+        *)
+        echo "Используй:"
+        echo "  build_generator"
+        echo "  run_generator"
+        echo "  create_local_data"
+        ;;
+esac
